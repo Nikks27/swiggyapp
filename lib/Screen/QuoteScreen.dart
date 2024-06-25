1d5c39a5434b5091f86a19aea12ac945387f6b7d
Nareshbhai Rabari <nareshrabari205@gmail.com>
2024-06-25 23:53:10 +0530

    daylog box

--- lib/Screen/QuoteScreen.dart
+++ lib/Screen/QuoteScreen.dart
@@ -1,17 +1,76 @@
 import 'package:flutter/material.dart';
 
-class QuoteScreen extends StatefulWidget {
-  const QuoteScreen({super.key});
+class NotificatinScreen extends StatefulWidget {
+  const NotificatinScreen({super.key});
 
   @override
-  State<QuoteScreen> createState() => _QuoteScreenState();
+  State<NotificatinScreen> createState() => _NotificatinScreenState();
 }
 
-class _QuoteScreenState extends State<QuoteScreen> {
+class _NotificatinScreenState extends State<NotificatinScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
+       body: Column(
+           children: [
+             Text('Get update on your ',style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
+             SizedBox(width:10,),
+             Text('order status  ',style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
+             SizedBox(width: 20,),
+             Text('Allow push notification to get real-time ',style: TextStyle(fontSize: 20),),
+             Text('updates on your order status',style: TextStyle(fontSize: 20),),
+             Row(
+               children: [
+                 Container(
+                   margin: EdgeInsets.only(top: 30,left: 30,right: 30),
+                   height: 400,
+                   width: 350,
+                   decoration: BoxDecoration(
+                     color: Colors.white,
+                     image: DecorationImage(
+                       image: AssetImage('Assest/flower-4905417_1920.jpg'),
+                       fit: BoxFit.cover,
+                     ),
+                   ),
+                 ),
+               ],
+             ),
+             Container(
+               margin: EdgeInsets.only(top: 30,right: 15,left: 15),
+               height: 60,
+               width: 360,
+               decoration: BoxDecoration(
+                 borderRadius: BorderRadius.circular(15),
+                 color: Colors.red,
+               ),
+               child: TextButton(
+                 onPressed: () => showDialog<String>(
+                   context: context,
+                   builder: (BuildContext context) => AlertDialog(
+                     title: Text('Allow Swiggy to send you notification?'),
+                     actions: [
+                       Row(
+                         children: [
+                           Text(' Allow '),
+                           TextButton(onPressed: () {
+                             Navigator.of(context).pop();
+                           }, child: Text('Dont Allow '))
+                         ],
+                       )
+                     ],
+                   ),
+                 ),
+                 child:  Text('Turn on Notification',style: TextStyle(fontSize: 20,color: Colors.white),),
+               ),
+               
+             ),
+             SizedBox(height: 10,),
+             TextButton(onPressed:() {
 
+             }, child: Text('Not now ',style: TextStyle(fontSize: 20),),)
+           ],
+       ),
     );
   }
 }
--- lib/Screen/spleshScreen.dart
+++ lib/Screen/spleshScreen.dart
@@ -17,7 +17,7 @@ class _SplashScreenState extends State<SplashScreen> {
   {
     Timer(Duration(seconds: 3),()
     {
-      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => QuoteScreen(),));
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => NotificatinScreen(),));
     },);
   }
   @override
